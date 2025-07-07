08da0c2f7b99fc976b0f17040eee3b77607e94ed
SUYASH JAGTAP <119748227+suyash3636@users.noreply.github.com>
2025-07-07 10:08:08 +0000

    Learning

--- 01_Basics/03_conversationDatatype.js
+++ 01_Basics/03_conversationDatatype.js
@@ -1,21 +1,73 @@
-// let strNum="10"
-// console.log(typeof strNum)
-// num = Number(strNum)
-// console.log(typeof num)
-// console.log(num)
 
-// Name= "alex"
-// console.log(typeof Name)
-// Boolname = Boolean(Name)
-// console.log(typeof Boolname)
-// console.log(Boolname)
+let score = "hitesh"
 
-/* 
-string to num
-"10" =>10
-"abc"=> NaN
-True =>1
-False => 0
-null => 0
+//console.log(typeof score);
+//console.log(typeof(score));
 
-*/
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+// console.log(gameCounter);
+ 
+
+
+x=10
+console.log(x++)
+y=10
+console.log(++y)
