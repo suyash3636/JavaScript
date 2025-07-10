680c83458e64c0e94b5d8671ef92df09d7a9a0ad
SUYASH JAGTAP <119748227+suyash3636@users.noreply.github.com>
2025-07-10 09:46:36 +0000

    Learning

---
+++ 02_Basics/01_Arrays.js
@@ -0,0 +1,38 @@
+my_arr = [1,2,3,4,5]
+// console.log(my_arr);
+// console.log(typeof my_arr);
+let arr1 = new Array(4,5,6,7)
+
+// console.log(arr1);
+
+
+// console.log(arr1[1]);
+// console.log(arr1.slice(0,5).reverse())
+
+
+// arr1.push(10)
+// console.log(arr1);
+
+// arr1.pop()
+// arr1.pop()
+
+
+// console.log(arr1);
+
+
+my_Arr = [1,2,3,4,7,8,9,10]
+// console.log(my_Arr.includes(6))
+// console.log(my_Arr.indexOf(9));
+
+
+// my_arr2= my_Arr.join()
+// console.log(my_arr2);
+// my_arr3 = my_Arr.join('-')
+// console.log(my_arr3);
+// console.log(typeof(my_arr3));
+
+My_array = [1,2,3,4,7,8,9,10]
+// console.log('A',My_array);
+// new_arr=My_array.splice(0,5)
+// console.log('A',My_array);
+// console.log('B',new_arr);
