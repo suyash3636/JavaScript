80cec4948da405bae9f00b09d51fb0e9b5200e62
SUYASH JAGTAP <119748227+suyash3636@users.noreply.github.com>
2025-07-10 07:11:52 +0000

    Learning

--- 01_Basics/06_Numbers.js
+++ 01_Basics/06_Numbers.js
@@ -39,5 +39,3 @@ const num2 = new Number(400)
 // let max =6
 // random_number=Math.floor(Math.random()* (max - min +1) + min )
 // console.log(random_number);
-
-
---
+++ 01_Basics/07_date.js
@@ -0,0 +1,22 @@
+// console.log(Date());
+let mydate = new Date()
+
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toISOString());
+
+
+// let mydate1=new Date("1,5,2025")
+// console.log(mydate1.toString());
+
+
+// let mydate2 = new Date(2025 , 1, 23 , 7 , 30 , 23)
+// // console.log(mydate2.toLocaleString());
+// console.log(mydate2.toDateString());
+
+// let timestamp_ =  Date.now()
+// console.log(timestamp_);
+
+let timestamp_1 = new Date();
+console.log(timestamp_1.getTime());
