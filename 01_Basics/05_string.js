8f17eb41cbb8c472f97b8ce899bf0e2ee36dee40
SUYASH JAGTAP <119748227+suyash3636@users.noreply.github.com>
2025-07-10 06:11:17 +0000

    Learning

---
+++ 01_Basics/05_string.js
@@ -0,0 +1,36 @@
+
+const name = "Alex"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Alex-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+// const newStringOne = "   hitesh    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://Ash-xplays-#004-pvp"
+
+console.log(url.replace('#004', 'pro'))
+
+console.log(url.includes('sundar'))
+
+console.log(url.split('-'));
---
+++ 01_Basics/06_Numbers.js
@@ -0,0 +1,43 @@
+const num = 400
+const num2 = new Number(400)
+// console.log(num);
+// console.log(num2);
+
+// console.log(num.toString());
+// console.log(typeof(num.toString()));
+
+// console.log(num.toString().length);
+// console.log(num.toFixed(2));
+
+
+// let somenumber= 14.34567
+// console.log(somenumber.toPrecision(3));
+// console.log(typeof somenumber.toPrecision(3));
+// console.log(typeof Number(somenumber.toPrecision(3)));
+
+// let Price=1000000
+
+// console.log(Price.toLocaleString());
+// console.log(Price.toLocaleString('en-IN'));
+
+//-------------------------------------- Math ----------------------------------------------------------------------
+
+// console.log(Math.abs(-10));
+// console.log(Math.round(5.5));
+// console.log(Math.round(5.6));
+// console.log(Math.ceil(5.1));
+// console.log(Math.floor(5.1));
+// console.log(Math.floor(5.9));
+// console.log(Math.sqrt(25));
+
+// console.log(Math.random());
+// console.log(Math.floor((Math.random() * 10) + 1));
+
+
+//dice
+// let min=1
+// let max =6
+// random_number=Math.floor(Math.random()* (max - min +1) + min )
+// console.log(random_number);
+
+
