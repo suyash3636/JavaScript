5c905f437bd9824cb724cd2942f8bece5c968712
SUYASH JAGTAP <119748227+suyash3636@users.noreply.github.com>
2025-07-15 11:40:29 +0000

    Learning

---
+++ 03_Basics/01_functions.js
@@ -0,0 +1,41 @@
+
+
+function sayMyName(){
+    console.log("S");
+    console.log("U");
+    console.log("Y");
+    console.log("A");
+    console.log("S");
+    console.log("H");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("suyash"))
+// console.log(loginUserMessage("suyash"))
+
